101db9fba5a15125dfb592274faf1cf60d09be8a
Sondre Vestad <sondre.vestad@tietoevry.com>
2022-07-22 13:55:43 +0200

    test orientation lock

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
 import './App.css';
 
+
+import DeviceOrientation, { Orientation } from 'react-screen-orientation'
 import { FullScreen, useFullScreenHandle } from "react-full-screen";
 import Controller from './Components/Controller'
 
 function App() {
   const handle = useFullScreenHandle();
 
-  window.screen.orientation.onchange = function(e) {
+  if (window?.screen?.orientation) window.screen.orientation.onchange = function(e) {
       console.log(`Orientation change. Orentiation ${window.screen.orientation.angle}`)
   }
 
   return (
       <div>
-        <button onClick={handle.enter} id="enter-fullscreen-button">
-          Enter fullscreen
-        </button>
+          <DeviceOrientation lockOrientation={'landscape'}>
+              {/* Will only be in DOM in landscape */}
+              <Orientation orientation='landscape' alwaysRender={false}>
+                  <div>
+                      <button onClick={handle.enter} id="enter-fullscreen-button">
+                           Fullscreen!
+                      </button>
 
-        <FullScreen handle={handle}>
-            <Controller />
+                      <FullScreen handle={handle}>
+                          <Controller />
 
-        </FullScreen>
+                      </FullScreen>
+                  </div>
+              </Orientation>
+              {/* Will stay in DOM, but is only visible in portrait */}
+              <Orientation orientation='portrait'>
+                  <div>
+                      <p>Please rotate your phone</p>
+                  </div>
+              </Orientation>
+          </DeviceOrientation>
       </div>
   );
 }
